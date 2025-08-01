fa1ffa0c91afce690c78409da44c53c7c3218794
Geetha Sri <geethu6997@gmail.com>
2025-08-01 20:30:05 +0530

    🔧 Updated

--- dashboard.py
+++ dashboard.py
@@ -9,12 +9,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 conn = psycopg2.connect(
-    host=os.getenv("localhost"),
-    port=os.getenv("port"),
-    dbname=os.getenv("dbname"),
-    user=os.getenv("user"),
-    password=os.getenv("password")
-) 
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
 
 cur = conn.cursor()
 
